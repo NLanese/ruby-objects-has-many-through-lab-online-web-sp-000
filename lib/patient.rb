033092d22009b57f919e371544634a893e98a410
Nick Lanese <nlanese21@gmail.com>
2020-08-22 20:41:35 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,7 +4,20 @@ class Patient
 
   def initialize(inputName)
     @name = inputName
+    @appointments = []
+    @doctors = []
     @@all << self
+  end
+
+  def new_appointment(date, doctor)
+
+  end
+
+  def add_appointment(app)
+    if !(self.appointments.include?(app))
+      @appointments << app 
+    end
+    return nil
   end
 
 end
