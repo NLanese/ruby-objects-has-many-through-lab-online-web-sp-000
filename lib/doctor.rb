65caba3127fd8f2fb4702c8cd3ff02bf0c74d73c
Nick Lanese <nlanese21@gmail.com>
2020-08-22 20:17:20 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,24 @@
+class Doctor
+  attr_accessor :name, :appointments
+  @@all = []
+
+  def initialize(inputName)
+    @name = inputName
+    @@all << self
+    @appointments = []
+  end
+
+  def new_appointment(date, patient)
+    newApp = Appointment.new(date, patient, self)
+    self.add_appointment(newApp)
+  end
+
+  def add_appointment(app)
+    if !(self.appointments.include?(app))
+      @appointments << app
+    end
+  end
+
+
+
+end
